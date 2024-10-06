16acd4000230cc3ae27ffe8da2dff898f5538d95
Ankita-Thakur20 <ankitathakur036238@gmail.com>
2024-10-06 10:44:24 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,121 @@
+
+
+
+
+let num = 10;
+
+for (i=1; i<=10; i++)
+
+{
+    console.log(num*i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// const table=(num)=>
+// {
+//     for(let a=1;a<=10;a++){
+    
+//         console.log(num*a);
+//     }
+// }
+// table(5);
+
+
+
+// const a=(num)=>{
+//     if(num %2===0){
+//         console.log("even")
+//     }
+//     else{
+//         console.log("odd")
+//     }
+//     }
+// a(3);
+
+
+// const sqr=(num)=>{
+// console.log(num*num*num);
+// }
+// sqr(3);
+
+// for(i=1;i<=5;i++){
+//     console.log("*");
+//        console.log("\n");
+// }
+
+
+// let arr=[1,2,5,7,9,8,"hi"];
+// for(i=0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+// let user={
+//     name:"a",
+//     age:23,
+     
+//     printname:function(){
+//         console.log(this.name);
+//     }
+
+// }
+// user.printname();
+               
+// let arr=[32,56,76,83,"hi"];
+// for (let arr1 of arr){
+//     console.log(arr1);
+//     break;
+// }
+
+// let users=[
+//     {
+//         id:1,
+//         name:"a",
+//         age:34,
+//         address:"mhow"
+    
+//     },
+//     {
+//         id:2,
+//         name:"b",
+//         age:23,
+//         address:"indore"
+
+//     },
+//     {
+//         id:3,
+//         name:"c",
+//         age:51,
+//         address:"rau"
+//     },
+//     {
+//         id:4,
+//         name:"d",
+//         age:30,
+//         address:"pthmpur"
+//     },
+// ]
+// for (let user of users){
+//     if(user.id===4){
+
+//     }
+// }
+// console.log(users[3].address)
+
+// for(let i=1;i<=5;i++){
+//     let a=" ";
+// for(let j=1;j<=5;j++){
+//     a+="*";
+// }
+// console.log(a);
+// }
